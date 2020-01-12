a1dfe9e1a93411d7f5c5691f0a2ed21d0c1c9cde
John Mark Goodson <john.mark.hat@gmail.com>
2020-01-11 19:43:41 -0500

    files set for caesar but signing off for now

---
+++ caesarCipher/caesar.js
@@ -0,0 +1,17 @@
+const caesar = {
+    add: function(num, num2) {
+        return num + num2;
+    },
+    subtract: function (num, num2) {
+        return num - num2; 
+    },
+    divide: function (num, num2) {
+        return num / num2;
+    },
+    multiply: function (num, num2) {
+        return num * num2;
+    }
+
+}
+
+module.exports = caesar;
