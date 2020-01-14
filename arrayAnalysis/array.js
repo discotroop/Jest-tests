17ff7af418ee43af7122ea034aa020a77868ea92
John Mark Goodson <john.mark.hat@gmail.com>
2020-01-14 14:34:30 -0500

    array analysis test and functions all written, this concludes our romp through jest... for now.

--- arrayAnalysis/array.js
+++ arrayAnalysis/array.js
@@ -1,6 +1,30 @@
-function capitalized(string) {
-    let result = string.split("");
-    result[0] = result[0].toUpperCase();
-    return result.join("");
+function calculateAverage(numArray) {
+  let result = 0;
+  for (let i = 0; i < numArray.length; i++) {
+    result += numArray[i];
   }
-  module.exports = capitalized;
+  return result / numArray.length;
+}
+
+function calculateMin(numArray) {
+  return Math.min(...numArray);
+}
+
+function calculateMax(numArray) {
+  return Math.max(...numArray);
+}
+
+function analysis(numArray) {
+    let result = {
+      average: 0,
+      min: 0,
+      max: 0,
+      length: 0,
+    }
+    result.average = calculateAverage(numArray);
+    result.min = calculateMin(numArray);
+    result.max = calculateMax(numArray);
+    result.length = numArray.length;
+    return result;
+  }
+  module.exports = analysis;
--- arrayAnalysis/array.test.js
+++ arrayAnalysis/array.test.js
@@ -1,5 +1,18 @@
-const capitalized = require('./array.js');
+const analysis = require('./array.js');
+const object = analysis([1, 8, 3, 4, 2, 6]);
 
-test('first letter is capitalized', () => {
-    expect(capitalized("bob")).toBe("Bob")
+test("calculates average value of num array", () => {
+    expect(object.average).toBe(4);
+});
+
+test("calculates smallest value in array", () => {
+    expect(object.min).toBe(1);
+});
+
+test("calculate largest value in array", () => {
+    expect(object.max).toBe(8);
+});
+
+test("calculates length of array", () => {
+    expect(object.length).toBe(6);
 });
