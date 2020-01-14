5bd92a24f27ddea8227ab73f063f5153181585fc
John Mark Goodson <john.mark.hat@gmail.com>
2020-01-14 14:16:51 -0500

    caesar is donezo, overly thoroughly probably.

--- caesarCipher/caesar.js
+++ caesarCipher/caesar.js
@@ -1,45 +1,41 @@
-// lowercase a = 97
-// lowercase z = 122;
-// uppercase A = 65;
-// uppercase Z = 90
-
-// String.fromCharCode(65) => A
-
-// split string into array of strings
-// convert all strings to char codes
-// if char code > 96 && < 123 run lowercase(char)
-// else if char > 64 && < 91 run uppercase(char)
-// else return char
-// join
-
-console.log("testing");
-
-function getCharCode(string) {
-    return string.charCodeAt(0);
+function selectCase(charCode) {
+    if (charCode > 96 && charCode < 123) {
+        return lowerCase(charCode);
+    } else if (charCode > 64 && charCode < 91) {
+        return upperCase(charCode);
+    } else {
+        // handles punctuation etc. 
+        return String.fromCharCode(charCode);
+    }
 }
 
 function lowerCase(charCode) {
     if (charCode >= 110) {
         result = charCode - 122 + 109;
-        console.log(result);
         return String.fromCharCode(result);
     } else {
         result = charCode + 13;
-        console.log(result);
         return String.fromCharCode(result);
     }
+}
 
+function upperCase(charCode) {
+    if (charCode >= 78) {
+        result = charCode - 90 + 77;
+        return String.fromCharCode(result);
+    } else {
+        result = charCode + 13;
+        return String.fromCharCode(result);
+    }
 }
 
 function caesar(string) {
     let workingString = string.split("")
     for (let i = 0; i < workingString.length; i++) {
         workingString[i] = workingString[i].charCodeAt(0);
-        console.log(workingString[i]);
-        workingString[i] = lowerCase(workingString[i]);
+        workingString[i] = selectCase(workingString[i]);
     }
     workingString = workingString.join("")
-    console.log(workingString);
 
     return workingString;
 
--- caesarCipher/caesar.test.js
+++ caesarCipher/caesar.test.js
@@ -10,8 +10,32 @@ test("multicharacter lowercase, 'ab' equals 'no'", () => {
 
 test("lowercase circles, 'z' = 'm' ", () => {
     expect(caesar("z")).toBe("m");
-})
+});
 
 test("lowercase alphabet", () => {
     expect(caesar("abcdefghijklmnopqrstuvwxyz")).toBe("nopqrstuvwxyzabcdefghijklm")
-})
+});
+
+test("uppercase 'A' equals 'N'", () => {
+    expect(caesar("A")).toBe("N");
+});
+
+test("multicharacter uppercase, 'AB' equals 'NO'", () => {
+    expect(caesar("AB")).toBe("NO");
+});
+
+test("uppercase circles, 'Z' = 'M' ", () => {
+    expect(caesar("Z")).toBe("M");
+});
+
+test("uppercase alphabet", () => {
+    expect(caesar("ABCDEFGHIJKLMNOPQRSTUVWXYZ")).toBe("NOPQRSTUVWXYZABCDEFGHIJKLM")
+});
+
+test("mixed case, 'AbXz' = 'NoKm' ", () => {
+    expect(caesar("AbXz")).toBe("NoKm");
+});
+
+test("skips punctuation, 'A.b' = 'N.o' ", () => {
+    expect(caesar("A.b")).toBe("N.o");
+});
