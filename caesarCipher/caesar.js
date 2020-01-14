c5c89adac2672797c8f92cc761661cec176295e1
John Mark Goodson <john.mark.hat@gmail.com>
2020-01-14 13:52:36 -0500

    lowercase works and has test coverage.

--- caesarCipher/caesar.js
+++ caesarCipher/caesar.js
@@ -14,28 +14,34 @@
 
 console.log("testing");
 
+function getCharCode(string) {
+    return string.charCodeAt(0);
+}
+
 function lowerCase(charCode) {
-    if (charCode > 110) {
-        charCode = charCode - 122 + 97;
-        return String.fromCharCode(charCode);
+    if (charCode >= 110) {
+        result = charCode - 122 + 109;
+        console.log(result);
+        return String.fromCharCode(result);
     } else {
-        charCode = charCode + 13;
-        return String.fromCharCode(charCode);
+        result = charCode + 13;
+        console.log(result);
+        return String.fromCharCode(result);
     }
 
 }
 
 function caesar(string) {
-    let stringArr = string.split("")
-    stringArr.forEach(function(string) {
-        string = string.charCodeAt(0);
-    })
-    stringArr.forEach(function(string) {
-        if (string > 96 && string < 123) {
-            string = lowerCase(string)
-        }
-    })
-    return stringArr.join("");
+    let workingString = string.split("")
+    for (let i = 0; i < workingString.length; i++) {
+        workingString[i] = workingString[i].charCodeAt(0);
+        console.log(workingString[i]);
+        workingString[i] = lowerCase(workingString[i]);
+    }
+    workingString = workingString.join("")
+    console.log(workingString);
+
+    return workingString;
 
 }
 
--- caesarCipher/caesar.test.js
+++ caesarCipher/caesar.test.js
@@ -3,3 +3,15 @@ const caesar = require('./caesar.js')
 test("lowercase 'a' equals 'n'", () => {
     expect(caesar("a")).toBe("n");
 });
+
+test("multicharacter lowercase, 'ab' equals 'no'", () => {
+    expect(caesar("ab")).toBe("no");
+});
+
+test("lowercase circles, 'z' = 'm' ", () => {
+    expect(caesar("z")).toBe("m");
+})
+
+test("lowercase alphabet", () => {
+    expect(caesar("abcdefghijklmnopqrstuvwxyz")).toBe("nopqrstuvwxyzabcdefghijklm")
+})
